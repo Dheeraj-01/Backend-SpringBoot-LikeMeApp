d1f6fcf031c22ee84d8a47c8e45fae608dae7ac3
Dheeraj Bhagchandani <dheerajbhagchandani@Dheerajs-MacBook-Air.local>
2023-06-29 00:12:26 +0530

    user flow complete

--- src/main/java/com/backend/likeme/controllers/UserControllers.java
+++ src/main/java/com/backend/likeme/controllers/UserControllers.java
@@ -8,6 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -18,7 +19,7 @@ public class UserControllers {
 
     // POST-create user
     @PostMapping("/")
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+    public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
         UserDto createUserDto = this.userService.createUser(userDto);
         return new ResponseEntity<>(createUserDto, HttpStatus.CREATED);
     }
@@ -26,7 +27,7 @@ public class UserControllers {
     // PUT- update user
 
     @PutMapping("/{userId}")
-    public ResponseEntity<UserDto> updateUser(@RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
+    public ResponseEntity<UserDto> updateUser(@Valid @RequestBody UserDto userDto, @PathVariable("userId") Integer uid) {
         UserDto updatedUser = this.userService.updateUser(userDto, uid);
         return ResponseEntity.ok(updatedUser);
     }
--- src/main/java/com/backend/likeme/payloads/UserDto.java
+++ src/main/java/com/backend/likeme/payloads/UserDto.java
@@ -5,14 +5,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 @Data
 @NoArgsConstructor
 @Getter
 @Setter
 public class UserDto {
-    private Integer id;
+    private int id;
+
+    @NotEmpty
+    @Size(min = 4, message = "Username must be min of 4 characters !!")
     private String name;
+
+    @Email(message = "Email address is not valid !!")
+    @NotEmpty(message = "Email is required !!")
     private String email;
+
+    @NotEmpty
+    @Size(min = 3, max = 10, message = "Password must be min of 3 chars and max of 10 chars !!")
+
+    @NotEmpty
     private String password;
+
+    @NotEmpty
     private String about;
 }
